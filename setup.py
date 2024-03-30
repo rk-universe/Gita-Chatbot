e8a29f060bfd2b3263eee6a5785e40a95f8d404b
rahul <kumawatrahul960@gmail.com>
2024-03-30 17:48:15 +0530

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'git Chatbot',
+    version= '0.0.0',
+    author= 'Rahul kumawat',
+    author_email= 'kumawatrahul960@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+prompt_template="""
+Provide insights and explanations based on the teachings of the Bhagavad Gita.
+If you're unsure or don't have an answer, it's okay to say that you don't know.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
